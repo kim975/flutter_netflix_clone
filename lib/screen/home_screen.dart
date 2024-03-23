98d626fd5475f951ccb45f26d017bec7033f0930
Kim YongMin <scilo200@gmail.com>
2024-03-23 15:23:05 +0900

    movie model 추가및 더미 데이터 생성

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:netplix_clone/model/model_movie.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Movie> movies = [
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': ' 사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    })
+  ];
+
   @override
   void initState() {
     super.initState();
@@ -34,22 +44,19 @@ class TopBar extends StatelessWidget {
               child: Text(
                 'TV 프로그램',
                 style: TextStyle(fontSize: 14),
-              )
-          ),
+              )),
           Container(
               padding: EdgeInsets.only(right: 1),
               child: Text(
                 '영화',
                 style: TextStyle(fontSize: 14),
-              )
-          ),
+              )),
           Container(
               padding: EdgeInsets.only(right: 1),
               child: Text(
                 '내가 찜한 콘덴츠',
                 style: TextStyle(fontSize: 14),
-              )
-          ),
+              )),
         ],
       ),
     );
